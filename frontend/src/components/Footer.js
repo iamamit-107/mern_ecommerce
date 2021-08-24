74ad88b7a4e2897ac039526c0b7d4c86aa441590
iamamit-107 <ahmed.amit107@gmail.com>
2021-08-24 20:46:02 +0600

    react-bootstrap, reactwatch theme and header footer setup

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container, Row } from "react-bootstrap";
+
+const Footer = () => {
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <p className="py-3 text-center">Copyright &copy; ProShop</p>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,36 @@
+import React from "react";
+import {
+  Navbar,
+  NavDropdown,
+  Nav,
+  Form,
+  Button,
+  FormControl,
+  Container,
+} from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="light" collapseOnSelect expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">Proshop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#home">
+                <i className="fas fa-shopping-cart" /> Cart
+              </Nav.Link>
+              <Nav.Link href="#link">
+                <i className="fas fa-user" />
+                Sign in
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
