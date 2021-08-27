df0f40f99e4b5feb6716a849f21927f6e9c9b33d
iamamit-107 <ahmed.amit107@gmail.com>
2021-08-27 21:32:33 +0600

    qty select added on details page

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,22 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { Button, Card, Col, Image, ListGroup, Row } from "react-bootstrap";
+import {
+  Button,
+  Card,
+  Col,
+  Form,
+  Image,
+  ListGroup,
+  Row,
+} from "react-bootstrap";
 import { Link, useParams } from "react-router-dom";
 import Ratings from "../components/Ratings";
 import { fetchSingleProduct } from "../redux/reducers/productDetailsReducer";
 
 const ProductScreen = () => {
+  const [qty, setQty] = useState(0);
+
   const { id } = useParams();
   const { product, loading, error } = useSelector(
     (state) => state.productDetails
@@ -61,6 +71,26 @@ const ProductScreen = () => {
                   </Col>
                 </Row>
               </ListGroup.Item>
+              {product.countInStock > 0 && (
+                <ListGroup.Item>
+                  <Row>
+                    <Col>QTY: </Col>
+                    <Col>
+                      <Form.Control
+                        as="select"
+                        value={qty}
+                        onChange={(e) => setQty(e.target.value)}
+                      >
+                        {[...Array(product.countInStock).keys()].map((x) => (
+                          <option key={x + 1} value={x + 1}>
+                            {x + 1}
+                          </option>
+                        ))}
+                      </Form.Control>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              )}
               <ListGroup.Item>
                 <Row>
                   <Button
