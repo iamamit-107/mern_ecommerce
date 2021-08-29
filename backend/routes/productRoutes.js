a888319cf0ebba2bcaa4ee2e187180a4a243cc98
iamamit-107 <ahmed.amit107@gmail.com>
2021-08-29 08:54:44 +0600

    cleanup router file by using controller separate

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,10 +2,11 @@
  * imports Start here
  */
 const express = require("express");
+const {
+  getProducts,
+  getProductById,
+} = require("../controllers/productController");
 const router = express.Router();
-
-const Product = require("../models/ProductModel");
-const asyncHandler = require("express-async-handler");
 
 /**
  * imports end here
@@ -16,32 +17,13 @@ const asyncHandler = require("express-async-handler");
  * @route: GET api/products
  * @access: Public
  */
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-    // throw new Error("Something wrong");
-  })
-);
+router.get("/", getProducts);
 
 /**
  * @desc: Fetch single product
  * @route: GET api/products/:id
  * @access: Public
  */
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Product not found");
-    }
-  })
-);
+router.get("/:id", getProductById);
 
 module.exports = router;
