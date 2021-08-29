868185cb771a54215e453c4a51e927d242107f4b
iamamit-107 <ahmed.amit107@gmail.com>
2021-08-29 10:00:21 +0600

    generate jwt token for user login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/UserModel");
+const generateToken = require("../utils/generateToken");
 
 /**
  * @desc: Auth Login
@@ -17,7 +18,7 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     });
   } else {
     res.status(401);
